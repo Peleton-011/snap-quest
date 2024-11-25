22b2e6e6e9ac0bc7ae0a133aa296913e3b87a0ec
Nico <nicodiazan@gmail.com>
2024-11-25 20:31:12 +0100

    Go back to old Grid component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Box, Button, Typography } from "@mui/material";
-import Grid from "@mui/material/Grid2"; 
+import Grid from "@mui/material/Grid"; 
 import CameraModal from "./components/CameraModal";
 
 interface Tile {
