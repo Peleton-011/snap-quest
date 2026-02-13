c8c103ffc1cca1e3f37630350c422c9e62aa436b
Nico Diaz <nicodiazan@gmail.com>
2026-02-13 12:08:03 +0100

    Finish up dexie db architecture

--- app/services/db.ts
+++ app/services/db.ts
@@ -1,8 +1,31 @@
 // db.ts
 import Dexie from "dexie";
 
+interface Photo {
+    id: number;
+    promptId: number;
+    promptSetId: number;
+    image: Blob;
+}
+
+interface PromptSet {
+    id: number;
+    name: string;
+    description: string;
+    isDefault: boolean;
+}
+
+interface Prompt {
+    id: number;
+    promptSetId: number;
+    fullPrompt: string;
+    shortPrompt: string;
+}
+
 class SnapQuestDB extends Dexie {
-	photos!: 
+	photos!: Dexie.Table<Photo>;
+    promptSets!: Dexie.Table<PromptSet>;
+    prompts!: Dexie.Table<Prompt>;
 
 	constructor() {
 		super("SnapQuestDB");
@@ -14,11 +37,6 @@ class SnapQuestDB extends Dexie {
 	}
 }
 
-const db = new Dexie("SnapQuestDB");
-db.version(1).stores({
-	photos: "++id, promptId, promptSetId",
-	promptSets: "++id, name, isDefault",
-	prompts: "++id, promptSetId",
-});
+const db = new SnapQuestDB();
 
 export default db;
