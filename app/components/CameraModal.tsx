059124a27e7bd890a974ffd77e6b7fbc18f90c38
Nico Diaz <nicodiazan@gmail.com>
2026-02-15 11:27:04 +0100

    Move CameraModal logic to work on mobile too

--- app/components/CameraModal.tsx
+++ app/components/CameraModal.tsx
@@ -8,7 +8,10 @@ import {
 	Typography,
 } from "@mui/material";
 
-import { Tile } from "@/app/types/types"
+import { Tile } from "@/app/types/types";
+
+import { Camera, CameraResultType } from "@capacitor/camera";
+import isNative from "../services/platform";
 
 interface CameraModalProps {
 	tile: Tile;
@@ -16,42 +19,74 @@ interface CameraModalProps {
 	onSave: (
 		id: number,
 		image: Blob | null,
-		orientation: "landscape" | "portrait"
+		orientation: "landscape" | "portrait",
 	) => void;
-    language: string;
+	language: string;
 }
 
-const CameraModal: React.FC<CameraModalProps> = ({ tile, onClose, onSave, language }) => {
+const CameraModal: React.FC<CameraModalProps> = ({
+	tile,
+	onClose,
+	onSave,
+	language,
+}) => {
 	const [preview, setPreview] = useState<string | null>(tile.image);
 
+	const determineOrientation = (src: string): "landscape" | "portrait" => {
+		// Determine orientation based on image dimensions
+		const img = new Image();
+		img.src = src;
+		let orientation: "landscape" | "portrait" = "landscape";
+		img.onload = () => {
+			orientation = img.width > img.height ? "landscape" : "portrait";
+		};
+		return orientation;
+	};
 	const handleCapture = async (
-		event: React.ChangeEvent<HTMLInputElement>
+		event: React.ChangeEvent<HTMLInputElement>,
 	) => {
 		const file = event.target.files?.[0];
 		if (file) {
 			const url = URL.createObjectURL(file);
 
-			// Determine orientation based on image dimensions
-			const img = new Image();
-			img.src = url;
-			img.onload = () => {
-				const orientation =
-					img.width > img.height ? "landscape" : "portrait";
-				setPreview(url);
-				onSave(tile.id, file, orientation);
-			};
+			setPreview(url);
+			onSave(tile.id, file, determineOrientation(url));
 		}
+	};
+
+	const handleNativeCapture = async () => {
+		// This is base64 unfortunately
+		const photo = await Camera.getPhoto({
+			resultType: CameraResultType.DataUrl,
+			quality: 90,
+		});
+
+		if (!photo || !photo.dataUrl) return;
+
+		await fetch(photo.dataUrl)
+			.then((res) => res.blob())
+			.then((blob) => {
+				setPreview(photo.webPath ?? null);
+
+				onSave(
+					tile.id,
+					blob,
+					determineOrientation(photo.webPath ?? ""),
+				);
+			});
 	};
 
 	const handleDelete = () => {
 		setPreview(null);
-        onSave(tile.id, null, "landscape");
+		onSave(tile.id, null, "landscape");
 	};
 
 	return (
 		<Dialog open onClose={onClose}>
 			<DialogContent className="modal">
-				<Typography variant="h6">{tile.prompt.fullPrompt[language]}</Typography>
+				<Typography variant="h6">
+					{tile.prompt.fullPrompt[language]}
+				</Typography>
 				<Box mt={2}>
 					{preview && (
 						<Box>
@@ -81,14 +116,18 @@ const CameraModal: React.FC<CameraModalProps> = ({ tile, onClose, onSave, langua
 				{!preview && (
 					<Button variant="contained" component="label">
 						Upload or Take a Photo
-						<input
-							type="file"
-							accept="image/*"
-							hidden
-							onChange={handleCapture}
-						/>
+						{isNative() ? (
+							<Button onClick={handleNativeCapture}>Close</Button>
+						) : (
+							<input
+								type="file"
+								accept="image/*"
+								hidden
+								onChange={handleCapture}
+							/>
+						)}
 					</Button>
-				)}
+				)}{" "}
 				<Button onClick={onClose}>Close</Button>
 			</DialogActions>
 		</Dialog>
