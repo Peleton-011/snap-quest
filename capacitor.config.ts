f0089013e940d3372b04f20afc5290fe6841edee
Nico Diaz <nicodiazan@gmail.com>
2026-02-15 11:46:51 +0100

    Config tweaks

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.snap_quest_pink.app',
   appName: 'snap-quest',
-  webDir: 'public'
+  webDir: 'out'
 };
 
 export default config;
