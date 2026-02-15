a07e2639152c250ea115e5f3ef4238340dea59e3
Nico Diaz <nicodiazan@gmail.com>
2026-02-15 11:35:17 +0100

    Add some more packages and config for mobile exporting

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.snap_quest_pink.app',
+  appName: 'snap-quest',
+  webDir: 'public'
+};
+
+export default config;
