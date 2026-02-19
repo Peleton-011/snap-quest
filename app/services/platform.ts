5fc911508ac4c9c2b8e118b1e4662cd8a43e3a9d
Nico Diaz <nicodiazan@gmail.com>
2026-02-19 18:30:27 +0100

    Fix naive platform util implementation to actually work

--- app/services/platform.ts
+++ app/services/platform.ts
@@ -1,3 +1,4 @@
-const isNative = () => typeof (window as any).Capacitor !== 'undefined';
+import { Capacitor } from "@capacitor/core";
+const isNative = () => Capacitor.isNativePlatform();
 
 export default isNative;
